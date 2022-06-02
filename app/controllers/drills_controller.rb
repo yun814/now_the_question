9fd1ed9973cbe7eed895bedd29a13fdf5ea03859
yun814 <magisuteru.magi@gmail.com>
2022-06-02 19:19:04 +0900

    drillの新規投稿機能を実装

--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -6,4 +6,20 @@ class DrillsController < ApplicationController
   def new
     @drill = Drill.new
   end
+
+  def create
+    @drill = Drill.new(drill_params)
+    if @drill.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def drill_params
+    params.require(:drill)
+          .permit(:title, :genre_id, :information)
+          .merge(user_id: current_user.id)
+  end
 end
--- app/models/drill.rb
+++ app/models/drill.rb
@@ -1,7 +1,7 @@
 class Drill < ApplicationRecord
   validates :title, presence: true, length: {minimum: 1, maximum: 20, message: "は20文字以内で入力してください"}
   validates :genre_id, presence: true, numericality: {other_than: 0, message: "can't be blank"}
-  validates :informaiton, presence: true, length: {minimum: 5, maximum: 1000, message: "は5文字以上1000文字以内で入力してください"}
+  validates :information, presence: true, length: {minimum: 5, maximum: 1000, message: "は5文字以上1000文字以内で入力してください"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
