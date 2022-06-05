076ad8efa87b73573f6f3b1ee5701dcf587bf0fb
yun814 <magisuteru.magi@gmail.com>
2022-06-05 18:56:00 +0900

    ドリルの新規投稿画面表示調整

--- app/models/drill.rb
+++ app/models/drill.rb
@@ -1,7 +1,7 @@
 class Drill < ApplicationRecord
   validates :title, presence: true, length: {minimum: 1, maximum: 20, message: "は20文字以内で入力してください"}
-  validates :genre_id, presence: true, numericality: {other_than: 0, message: "can't be blank"}
-  validates :information, presence: true, length: {minimum: 1, maximum: 1000, message: "は1000文字以内で入力してください"}
+  validates :genre_id, numericality: {other_than: 0, message: "を選択してください"}
+  validates :information, presence: true, length: {minimum: 1, maximum: 175, message: "は175文字以内で入力してください"}
 
   has_many :quizzes
   belongs_to :user
