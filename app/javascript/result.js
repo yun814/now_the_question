cfa99f71c8321fb73b95017a9c8645a553212215
yun814 <magisuteru.magi@gmail.com>
2022-06-12 06:20:53 +0900

    結果画面でのブラウザバックを禁止

--- app/javascript/result.js
+++ app/javascript/result.js
@@ -1,21 +1,26 @@
 function result() {
   const confirmBtn = document.getElementById('confirm-btn');
   if (!confirmBtn) return null;
-
+  
+  history.pushState(null, null, location.href);
+  window.addEventListener('popstate', () => {
+    history.go(1);
+  });
+  
   confirmBtn.addEventListener('mouseover', () => {
     confirmBtn.style.boxShadow = '0 0 5px rgb(150, 150, 150)'
     confirmBtn.style.cursor = 'pointer';
   });
-
+  
   confirmBtn.addEventListener('mouseout', () => {
     confirmBtn.style.boxShadow = ''
     confirmBtn.style.cursor = '';
   });
-
+  
   confirmBtn.addEventListener('click', () => {
     const quizzes = document.querySelectorAll('.quiz');
     const hiddenBtn = document.getElementById('hidden-btn');
-
+    
     quizzes.forEach((quiz) => {
       quiz.removeAttribute('hidden');
     });
