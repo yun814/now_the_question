ef543c660f62fe090c6e7f9b0ffa65ea7b04e379
yun814 <magisuteru.magi@gmail.com>
2022-06-12 05:41:55 +0900

    結果画面でクイズの正解をボタンをクリックすることで表示するように変更

---
+++ app/javascript/result.js
@@ -0,0 +1,26 @@
+function result() {
+  const confirmBtn = document.getElementById('confirm-btn');
+
+  confirmBtn.addEventListener('mouseover', () => {
+    confirmBtn.style.boxShadow = '0 0 5px rgb(150, 150, 150)'
+    confirmBtn.style.cursor = 'pointer';
+  });
+
+  confirmBtn.addEventListener('mouseout', () => {
+    confirmBtn.style.boxShadow = ''
+    confirmBtn.style.cursor = '';
+  });
+
+  confirmBtn.addEventListener('click', () => {
+    const quizzes = document.querySelectorAll('.quiz');
+    const hiddenBtn = document.getElementById('hidden-btn');
+
+    quizzes.forEach((quiz) => {
+      quiz.removeAttribute('hidden');
+    });
+    hiddenBtn.removeAttribute('hidden');
+    confirmBtn.style.display = 'none';
+  });
+};
+
+window.addEventListener('load', result)
