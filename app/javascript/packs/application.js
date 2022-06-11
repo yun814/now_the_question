836dfad8582b0b4251a3c5f00bdcbbfa7f839b18
yun814 <magisuteru.magi@gmail.com>
2022-06-11 15:22:17 +0900

    クイズにカーソルを合わせた時の見た目変更を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../answer")
 require("../link")
 require("../drill")
+require("../quiz")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/quiz.js
@@ -0,0 +1,22 @@
+function quiz() {
+  // クイズ要素を取得し、配列化
+  const quizzes = document.querySelectorAll('.quiz')
+  let quizzesArray = Array.prototype.slice.call(quizzes);
+  // クイズ追加フォームを取得
+  const quizForm = document.getElementById('quiz-form');
+  // クイズ追加フォームが存在する場合はクイズ配列に追加
+  if (quizForm) quizzesArray.push(quizForm);
+
+  quizzesArray.forEach((quiz) => {
+    // クイズにカーソルを合わせると影を表示
+    quiz.addEventListener('mouseover', () => {
+      quiz.style.boxShadow = '0 0 10px rgb(180, 180, 180)'
+    });
+    // クイズからカーソルを離すと元に戻る
+    quiz.addEventListener('mouseout', () => {
+      quiz.style.boxShadow = ''
+    });
+  });
+};
+
+window.addEventListener('load', quiz)
