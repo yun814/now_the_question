cdd307ddd54b29ea6cd004c78a2a589040b73b91
yun814 <magisuteru.magi@gmail.com>
2022-06-12 17:22:23 +0900

    recordの計算方法を変更

--- app/models/result_records.rb
+++ app/models/result_records.rb
@@ -3,8 +3,6 @@ class ResultRecords
   attr_accessor :times, :number_of_questions, :number_of_correct_answers, :correct_answer_rate, :user_id, :drill_id,
                 :first_number_of_questions, :first_number_of_correct_answers, :first_correct_answer_rate, :all_number_of_questions, :all_number_of_correct_answers, :all_correct_answer_rate
 
-  
-
   validates :times, presence: true
   validate :add_error_times
   validates :number_of_questions, presence: true
@@ -20,20 +18,38 @@ class ResultRecords
   private
   def record_hash
     user = User.find(user_id)
-    first_number_of_questions = user.results.where(times: 1).sum(:number_of_questions)
-    first_number_of_correct_answers = user.results.where(times: 1).sum(:number_of_correct_answers)
-    first_correct_answer_rate = (first_number_of_correct_answers.to_f / first_number_of_questions.to_f) * 100
-    all_number_of_questions = user.results.sum(:number_of_questions)
-    all_number_of_correct_answers = user.results.sum(:number_of_correct_answers)
-    all_correct_answer_rate = (all_number_of_correct_answers.to_f / all_number_of_questions.to_f) * 100
-    record_hash = {
-      first_number_of_questions: first_number_of_questions,
-      first_number_of_correct_answers: first_number_of_correct_answers,
-      first_correct_answer_rate: first_correct_answer_rate,
-      all_number_of_questions: all_number_of_questions,
-      all_number_of_correct_answers: all_number_of_correct_answers,
-      all_correct_answer_rate: all_correct_answer_rate
-    }
+
+    if times == "1"
+      first_number_of_questions = user.record.first_number_of_questions + number_of_questions.to_i
+      first_number_of_correct_answers = user.record.first_number_of_correct_answers + number_of_correct_answers.to_i
+      first_correct_answer_rate = (first_number_of_correct_answers.to_f / first_number_of_questions.to_f) * 100
+      all_number_of_questions = user.record.all_number_of_questions + number_of_questions.to_i
+      all_number_of_correct_answers = user.record.all_number_of_correct_answers + number_of_correct_answers.to_i
+      all_correct_answer_rate = (all_number_of_correct_answers.to_f / all_number_of_questions.to_f) * 100
+      record_hash = {
+        first_number_of_questions: first_number_of_questions,
+        first_number_of_correct_answers: first_number_of_correct_answers,
+        first_correct_answer_rate: first_correct_answer_rate,
+        all_number_of_questions: all_number_of_questions,
+        all_number_of_correct_answers: all_number_of_correct_answers,
+        all_correct_answer_rate: all_correct_answer_rate
+      }
+    else
+      first_number_of_questions = user.record.first_number_of_questions
+      first_number_of_correct_answers = user.record.first_number_of_correct_answers
+      first_correct_answer_rate = user.record.first_correct_answer_rate
+      all_number_of_questions = user.record.all_number_of_questions + number_of_questions.to_i
+      all_number_of_correct_answers = user.record.all_number_of_correct_answers + number_of_correct_answers.to_i
+      all_correct_answer_rate = (all_number_of_correct_answers.to_f / all_number_of_questions.to_f) * 100
+      record_hash = {
+        first_number_of_questions: first_number_of_questions,
+        first_number_of_correct_answers: first_number_of_correct_answers,
+        first_correct_answer_rate: first_correct_answer_rate,
+        all_number_of_questions: all_number_of_questions,
+        all_number_of_correct_answers: all_number_of_correct_answers,
+        all_correct_answer_rate: all_correct_answer_rate
+      }
+    end
   end
 
   def add_error_times
