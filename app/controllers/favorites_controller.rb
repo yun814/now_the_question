6240f6b6f69ecd618269d2d690702918945c9825
yun814 <magisuteru.magi@gmail.com>
2022-06-14 23:08:45 +0900

    いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,12 @@ class FavoritesController < ApplicationController
   before_action :set_drill
 
   def create
-    @favorite = Favorite.new(user_id: current_user.id, drill_id: @drill.id)
-    @favorite.save
-    redirect_to drill_results_path(@drill)
+    Favorite.create(user_id: current_user.id, drill_id: @drill.id)
   end
   
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, drill_id: @drill.id)
     @favorite.destroy
-    redirect_to drill_results_path(@drill)
   end
 
   private
