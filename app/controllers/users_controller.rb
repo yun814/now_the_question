8ce7ff4a9deed9ba8fa96bb309c7285d0d58cd41
yun814 <magisuteru.magi@gmail.com>
2022-06-16 01:00:37 +0900

    ユーザー詳細画面の表示を調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @drills = @user.drills.order(updated_at: 'DESC')
+    @public_drills = @user.drills.order(updated_at: 'DESC').where(status: 1)
     @results = @user.results.order(created_at: 'DESC').limit(5)
     @genres = Genre.all
     if user_signed_in?
@@ -11,12 +11,15 @@ class UsersController < ApplicationController
       end
 
       @times_array_for_user = []
-      @user.drills.each do |drill|
-        @times_array_for_record << drill.results.where(user_id: current_user.id).count
+      @public_drills.each do |drill|
+        @times_array_for_user << drill.results.where(user_id: current_user.id).count
       end
 
       @side_results = current_user.results.order(created_at: 'DESC').limit(5)
 
+      if @user.id == current_user.id
+        @all_drills = @user.drills.order(updated_at: 'DESC')
+      end
     end
   end
 end
