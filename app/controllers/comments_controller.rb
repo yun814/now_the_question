6ca9af88c4c8a4d3c2f81f2c2d0482ee077cd6fb
yun814 <magisuteru.magi@gmail.com>
2022-06-16 20:06:46 +0900

    コメント機能・削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to drill_path(comment.drill)
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to drill_path(comment.drill)
+  end
+
+  private
+  def comment_params
+    params.require(:comment)
+          .permit(:comment)
+          .merge(user_id: current_user.id, drill_id: params[:drill_id])
+  end
 end
--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -25,7 +25,9 @@ class DrillsController < ApplicationController
   def show
     @quiz = Quiz.new
     @quizzes = @drill.quizzes
+    @comments = @drill.comments.order(created_at: 'DESC').includes(:user)
     if user_signed_in?
+      @comment = Comment.new
       @results = @drill.results.order(created_at: 'DESC').where(user_id: current_user.id)
       @favorite = Favorite.find_by(user_id: current_user.id, drill_id: @drill.id)
     end
