058c3ff2bff2ff4dfe770e2979ee2aaec118ff80
yun814 <magisuteru.magi@gmail.com>
2022-06-16 13:01:56 +0900

    genre表示画面へのリンクのパスを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @public_drills = @user.drills.order(updated_at: 'DESC').where(status: 1)
     if user_signed_in? && @user.id == current_user.id
       @all_drills = @user.drills.order(updated_at: 'DESC')
-      
-    elsif user_signed_in?
+    end
+    
+    if user_signed_in?
       @side_results = current_user.results.order(created_at: 'DESC').limit(5)
     end
   end
