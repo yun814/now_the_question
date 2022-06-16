282831cc84692a73458645873b620f3f92f51b10
yun814 <magisuteru.magi@gmail.com>
2022-06-16 21:09:09 +0900

    表示を調整

---
+++ app/javascript/comment.js
@@ -0,0 +1,24 @@
+function search() {
+  const commentDisplayBtn = document.getElementById('comment-display-btn');
+  const commentHidden = document.getElementById('comment-hidden');
+  const hidden = document.querySelectorAll('.hidden');
+
+  commentDisplayBtn.addEventListener('mouseover', () => {
+    commentDisplayBtn.style.boxShadow = '0 0 5px rgb(150, 150, 150)';
+    commentDisplayBtn.style.cursor = 'pointer';
+  });
+  
+  commentDisplayBtn.addEventListener('mouseout', () => {
+    commentDisplayBtn.style.boxShadow = '';
+    commentDisplayBtn.style.cursor = '';
+  });
+  
+  commentDisplayBtn.addEventListener('click', () => {
+    commentHidden.style.display = 'contents';
+    hidden.forEach((h) => {
+      h.style.display = 'none';
+    })
+  });
+};
+
+window.addEventListener('load', search);
