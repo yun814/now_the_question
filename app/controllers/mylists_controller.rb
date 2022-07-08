b1730e8615ee5683e44dfd1fcd9062cf3103d6df
yun814 <magisuteru.magi@gmail.com>
2022-07-08 16:32:01 +0900

    マイリスト削除・クイズ集登録解除を本人以外ができないよう修正

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -1,4 +1,7 @@
 class MylistsController < ApplicationController
+  before_action :set_mylist, only: [:show, :destroy, :remove]
+  before_action :move_to_index, only: [:destroy, :remove]
+
   def create
     @mylist = Mylist.new(mylist_params)
     if @mylist.save
@@ -26,24 +29,31 @@ class MylistsController < ApplicationController
     if user_signed_in?
       @side_results = current_user.results.order(created_at: 'DESC').limit(5)
     end
-
-    @mylist = Mylist.find(params[:id])
   end
 
   def destroy
-    @mylist = Mylist.find(params[:id])
     @mylist.destroy
   end
-
+  
   def remove
     @mylist_drill = MylistDrill.find_by(mylist_id: params[:mylist_id], drill_id: params[:drill_id])
     @mylist_drill.destroy
   end
-
+  
   private
   def mylist_params
     params.require(:mylist)
-          .permit(:title)
-          .merge(user_id: current_user.id)
+    .permit(:title)
+    .merge(user_id: current_user.id)
+  end
+  
+  def set_mylist
+    @mylist = Mylist.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @mylist.user_id
+      redirect_to root_path
+    end
   end
 end
