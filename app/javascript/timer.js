4a3437c4a65dbe6b335b946ec381c55dd4d400c0
yun814 <magisuteru.magi@gmail.com>
2022-07-16 13:55:16 +0900

    時間切れ時の処理を追加

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -1,15 +1,23 @@
+// 1秒毎にtimer要素の中身を書き換える関数
 function countDown(timer) {
   let timerNum = Number(timer.innerHTML)
   timerNum -= 1
   timer.innerHTML = String(timerNum)
-  console.log(timerNum);
+
+  // 時間切れ時の処理
+  if (timerNum == -1) {
+    const finishBtn = document.getElementById('finish-btn')
+    alert('※時間切れです。')
+    finishBtn.click()
+  }
 }
 
 function timer() {
-  // 回答終了ボタンが存在するページのみ処理を実行
+  // timer要素が存在するページのみ処理を実行
   const timer = document.getElementById('timer');
   if (!timer) return null;
 
+  // 1秒毎にcountDown関数を実行する
   setInterval(countDown, 1000, timer)
 }
 
